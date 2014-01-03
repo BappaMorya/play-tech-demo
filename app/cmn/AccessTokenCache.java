06c01586c3ea66dc7d9ab19e09c576cee3944e2a
Sid <siddharth.god@gmail.com>
2014-01-03 10:14:48 +0530

    Indirect session timeout

--- app/cmn/AccessTokenCache.java
+++ app/cmn/AccessTokenCache.java
@@ -1,16 +1,42 @@
 package cmn;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+import play.Logger;
+import play.Play;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
 
 public class AccessTokenCache {
 	
 	private static final AccessTokenCache instance = new AccessTokenCache();
 	
-	private static final Map<String, String> tokenMap = new ConcurrentHashMap<String, String>();
+	private static Cache<String, String> tokenCache = null;
 	
 	private AccessTokenCache() {
+		long cacheTimeout = 0L;
+		try {
+			cacheTimeout = Long.parseLong(Play.application().configuration().getString("cacheTimeout"));
+		} catch (NumberFormatException nfe) {
+			cacheTimeout = 10;
+		}
 		
+		RemovalListener<String, String> listener = new RemovalListener<String, String>() {
+			
+			@Override
+			public void onRemoval(RemovalNotification<String, String> removal) {
+				Logger.debug("Expiring Uid " + removal.getKey());
+				UserPostStore.getInstance().removeUser(removal.getKey());
+			}
+		};
+		
+		tokenCache = CacheBuilder.newBuilder()
+				.expireAfterWrite(cacheTimeout, TimeUnit.MINUTES)
+				.removalListener(listener)
+				.build();
 	}
 	
 	public static final AccessTokenCache getInstance() {
@@ -18,15 +44,15 @@ public class AccessTokenCache {
 	}
 	
 	public void addToken(String uid, String accessToken) {
-		tokenMap.put(uid, accessToken);
+		tokenCache.put(uid, accessToken);
 	}
 	
 	public String getToken(String uid) {
-		return tokenMap.get(uid);
+		return tokenCache.getIfPresent(uid);
 	}
 	
 	public void removeToken(String uid) {
-		tokenMap.remove(uid);
+		tokenCache.invalidate(uid);
 	}
 
 }
--- build.sbt
+++ build.sbt
@@ -6,7 +6,8 @@ libraryDependencies ++= Seq(
   javaJdbc,
   javaEbean,
   cache,
-  "com.restfb" % "restfb" % "1.6.12"
+  "com.restfb" % "restfb" % "1.6.12",
+  "com.google.guava" % "guava" % "15.0"
 )     
 
 play.Project.playJavaSettings
